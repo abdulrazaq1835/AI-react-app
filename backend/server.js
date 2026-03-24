57bc057c314382fb79e1c4c7447a4d05d906182c
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-03-24 15:43:25 +0530

    build ready

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 connectDb()
 app.use('/api', router)
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("API is running...");
 });
 
 app.listen(PORT, () => {
