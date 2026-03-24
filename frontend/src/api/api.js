3b3d70cc141466d10f218958918d94b856a6ed99
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-03-24 15:51:34 +0530

    env setup

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,18 +1,36 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000/api'
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : `${import.meta.env.VITE_API_URL}/api`;
+
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+
+
 
 export const askAi = async (prompt) => {
-  const res = await axios.post(`${BASE_URL}/ask-ai`, { prompt })
-  return res.data.answer
-}
+  try {
+    const res = await api.post("/ask-ai", { prompt });
+    return res.data.answer;
+  } catch (error) {
+    console.error("Error in askAi:", error);
+    return "Something went wrong";
+  }
+};
+
 
 export const saveData = async (prompt, response) => {
-  const res = await axios.post(`${BASE_URL}/save`, { prompt, response })
-  return res.data
-}
-
-export const getHistory = async () => {
-  const res = await axios.get(`${BASE_URL}/history`)
-  return res.data
-}
+  try {
+    const res = await api.post("/save", { prompt, response });
+    return res.data;
+  } catch (error) {
+    console.error("Error in saveData:", error);
+    return null;
+  }
+};
