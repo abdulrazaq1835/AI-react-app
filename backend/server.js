b6236956a6474d1c933830d13ea781761fe10756
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-03-24 15:38:11 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ app.use(express.json())
 
 connectDb()
 app.use('/api', router)
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`)
